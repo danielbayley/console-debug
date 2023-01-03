9fe720cdd828d6480629d9ba3610ed21985723e8
Daniel Bayley <daniel.bayley@me.com>
2023-01-03 14:17:37 +0000

    Add browser `window` functionality

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const debug = process.env.DEBUG || process.env.NODE_ENV === 'development'
 
 const console = { ...global.console }
 
-console.debug = (...args) => {
+if (this?.window) console.debug.bind(window.console)
+else console.debug = (...args) => {
   if (debug) global.console.debug.apply(this, args)
 }
 
